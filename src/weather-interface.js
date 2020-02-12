463da1d2f421777056d5aeefafd5e2ac3bfcbe5f
Adela Darmansyah and Krista Rutz <adela@>
2020-02-12 09:50:54 -0800

    update api call to use async functions and await keyword

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(jsonifiedResponse) {
-        getElements(jsonifiedResponse);
-      });
+    asyncApiCall();
+
+    async function asyncApiCall(){
+      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+      let jsonifiedResponse = await response.json();
+      getElements(jsonifiedResponse);
+    }
 
     const getElements = function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
