56cef53ba5728d4a067fadad3fa6025c56bc028f
Student <example@example.com>
2020-02-13 15:54:15 -0800

    Use chains of promises

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -2,22 +2,58 @@ import $ from 'jquery';
 import 'bootstrap' ; 
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
-import { WeatherService } from '../src/weather-service';
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    (async () => {
-      let weatherService = new WeatherService();
-      const response = await weatherService.getWeatherByCity(city);
-      getElements(response);
-    })();
+    function weatherCall() {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.weatherKey}`;
 
-    const getElements = function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+
+        request.open("GET", url, true);
+        request.send();
+      });
     }
+
+    function giphyCall(humidity) {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `http://api.giphy.com/v1/gifs/search?q=${humidity}&api_key=${process.env.giphyKey}&limit=5`;
+
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
+
+    weatherCall()
+    .then(function(response) {
+      let body = JSON.parse(response);
+      let humidity = body.main.humidity;
+      return giphyCall(humidity);
+    })
+    .then(function(response) {
+      let giphyResponse = JSON.parse(response);
+      let image = giphyResponse["data"][0]["images"]["downsized"]["url"];
+      $(".showImage").html(`<img src ='${image}'>`);
+    });
   });
 });
